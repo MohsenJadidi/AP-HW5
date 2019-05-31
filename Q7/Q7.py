773bc5bce9d5f44ba14d3e3e3fa1f9eafe2a670e
Mohammad Mohsen Jadidi <47446864+MohsenJadidi@users.noreply.github.com>
2019-05-31 14:33:51 +0430

    Update Q7.py

--- Q7/Q7.py
+++ Q7/Q7.py
@@ -1,12 +1,4 @@
+import sys
+from numpy import asarray, sort, unique
 
-
-# A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-# A = [3, 4, 1, 37, 21, 18, 23, 21, 27, 22, 43, 21]
-# A = set(input('Enter : '))
-# print(' '.join(set([str(i) for i in input().split() if ((i % 6 == 0) and ((A.index(i)+1) % 6) == 0)])))
-
-print(' '.join(list(filter((lambda x: int(x) % 6 == 0), input().split()))))
-
-
-#print(' '.join(list(set([i for i in input().split() if (int(i) % 6 == 0)]))))
-
+print(' '.join([str(st)for st in list(sort(unique(asarray([int(sys.argv[i+1]) for i in range(len(sys.argv)-1) if (int(sys.argv[i+1]) % 6 == 0) and (i+1) % 6 == 0]))))]))
